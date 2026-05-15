28ff5c0799f7ab12bd2b295ad7cf5ac89210b405
hecker-01 <101524930+Hecker-01@users.noreply.github.com>
2026-05-15 21:08:32 +0200

    version number bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = gitCommitCount
-        versionName = "26.5.3"
+        versionName = "26.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
